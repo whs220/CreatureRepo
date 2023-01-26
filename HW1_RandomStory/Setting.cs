19784ee7ee45add0a01307a53106006f3fdf3d7e
Jake <jakew@LAPTOP-DQR5CE9F>
2023-01-26 12:48:53 -0800

    Completed the setting class

--- HW1_RandomStory/Setting.cs
+++ HW1_RandomStory/Setting.cs
@@ -1,12 +1,48 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿
+
+using System.Globalization;
 
 namespace HW1_RandomStory
 {
     internal class Setting
     {
+        // ==== FEILDS ====
+
+        private string strLocation;
+        private string strSeason;
+
+
+        // ==== PROPERTIES ====
+
+        /// <summary>
+        /// Returns the location so accessible in the main method
+        /// </summary>
+        public string StrLocation
+        {
+            get
+            {
+                return strLocation;
+            }
+        }
+
+        /// <summary>
+        /// Returns the season
+        /// </summary>
+        public string StrSeason
+        {
+            get
+            {
+                return strSeason;
+            }
+        }
+
+
+        // ==== Constructor ====
+        public Setting(string location, string season)
+        {
+            this.strLocation = location;
+            this.strSeason = season;
+
+        }
     }
 }
