7f6b3e32253c96586f4d54ebde8d1c0eb6c01893
Will Slyman <whs1585@rit.edu>
2023-01-25 20:32:28 -0500

    Created the files

---
+++ Homework1/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace HW1_RandomStory
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
