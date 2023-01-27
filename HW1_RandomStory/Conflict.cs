9d4b49ab554f3c699e7ed88588bb9653d27369b6
Will Slyman <whs1585@rit.edu>
2023-01-27 18:24:54 -0500

    Finished conflict

--- HW1_RandomStory/Conflict.cs
+++ HW1_RandomStory/Conflict.cs
@@ -1,4 +1,9 @@
-﻿using System;
+﻿// Will Slyman
+// 1/27/2023
+// Class: Conflict
+// Holds information about the conflict of the random story!!
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,21 +11,22 @@ using System.Threading.Tasks;
 
 namespace HW1_RandomStory
 {
+    public enum ending
+    {
+        Happy,
+        Tragic,
+        Twist,
+        Cliffhanger,
+        Strange,
+        Lame
+    }
+
     internal class Conflict
     {
         //  Fields
         private string conflict;
-        private string resolution;
         private ending end;
-        public enum ending
-        {
-            Happy,
-            Tragic,
-            Twist,
-            Cliffhanger,
-            Strange,
-            Lame
-        }
+        
 
         /// <summary>
         /// Parameterized constructor to make a conflict!
@@ -28,32 +34,10 @@ namespace HW1_RandomStory
         /// <param name="conflict">String of the conflict!</param>
         /// <param name="resolution">String of the resolution!</param>
         /// <param name="end">Type of ending to append!</param>
-        public Conflict(string conflict, string resolution, ending end)
+        public Conflict(string conflict, ending end)
         {
             this.conflict = conflict;
-            this.resolution = resolution;
             this.end = end;
-        }
-
-        /// <summary>
-        /// Returns the conflict and resolution
-        /// </summary>
-        /// <returns></returns>
-        public string[] GetConflict()
-        {
-            string[] data = { conflict, GetEnding() };
-            return data;
-        }
-
-        private string GetEnding()
-        {
-            switch (end)
-            {
-                case 0:
-                    return "Happy";
-            }
-
-            return "";
         }
     }
 }
