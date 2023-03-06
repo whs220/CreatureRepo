f6d8f9dbecd097dcd943998b0b3701fd5c5b94b2
jlw6563 <jlw6563@MSS2175-04>
2023-03-06 14:45:08 -0500

    Created gameobjexy

---
+++ Slime Game/Slime Game/GameObject.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace Slime_Game
+{
+    internal class GameObject
+    {
+        // ==== Field ====
+        Texture2D texture;
+        Rectangle position;
+
+
+        // ==== Constuctor ====
+
+        /// <summary>
+        /// Creates a game object
+        /// </summary>
+        /// <param name="texture">The texture</param>
+        /// <param name="pos">Rectangle pos</param>
+        public GameObject(Texture2D texture, Rectangle pos)
+        {
+            this.texture = texture;
+            position = pos;
+        }
+
+
+        // ==== Method ====
+        
+        /// <summary>
+        /// Draws gameobject
+        /// </summary>
+        /// <param name="sb">The sprite batch</param>
+        public virtual void Draw(SpriteBatch sb)
+        {
+            sb.Draw(texture, position, Color.White);
+        }
+        
+    }
+}
