5ee4549235e70d7055a7a1f066f79273247269ac
Josephine Caradonna <jac9513@rit.edu>
2023-03-06 15:03:29 -0500

    hi guys its  e josie here waht s usp yall

---
+++ Slime Game/Slime Game/Level.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Slime_Game
+{
+    internal class Level
+    {
+        //Fields
+        string fileName;
+        List<GameObject> gameObjects;
+        List<Tile> tiles;
+        List<Collectable> collectables;
+
+        //Method
+        public List<GameObject> ReadLevel()
+        {
+            return gameObjects;
+        } 
+
+    }
+}
