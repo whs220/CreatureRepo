a9b5aa181f27b6c0133cf41d7be33153a90ae7ee
Will Slyman <whs1585@rit.edu>
2023-04-12 15:17:43 -0400

    spring support added!!

---
+++ Slime Game/Slime Game/Spring.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Input;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Slime_Game
+{
+    /// <summary>
+    /// Spring Class
+    /// Gives player a bounce on collision
+    /// </summary>
+    internal class Spring : GameObject
+    {
+        // ===== Fields =====
+        public Spring(Rectangle rect) : base(null, new Rectangle())
+        {
+            this.position = rect;
+            this.texture = Art.Instance.LoadTexture2D("debug_solid");
+        }
+    }
+}
